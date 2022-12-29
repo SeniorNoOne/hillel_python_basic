7feb308269865e49eac5d503ea961ece836ce440
Martyniuk Vadym <martynyukvadum@gmail.com>
2022-12-29 23:12:20 +0200

    Homework_15
    Довго працював, але додав наступні фічі:
    1. Квитки зчитуються теж з CSV файла і при ініціалізації маршруту продані квитки теж враховуються
    2. При ініціалізації маршруту перевіряється, чи маршрут вже відправився. Якщо так, то створюється новий маршрут, який перенесений на тиждень вперед відносно теперішньої дати. При цьому перевіряється, чи новий маршрут вже є в CSV файлі. Якщо він є в файлі, то новий маршрут не додається у файл
    3. Створив класи для роботи з CSV файлами, а також фабричний метод, який в залежності від режиму роботи з файлом створює відповідний тип об'єкту
    4. Привів в порядок імпорти
    5. Виніс всі "магічні числа" і константи в config.py
    6. Додав контекстний менеджер для класів, які працюють з CSV файлами, та BusStation
    7. Позбувся від лишніх методів

--- src/bookticket/classes/custom_exeptions.py
+++ src/bookticket/classes/custom_exeptions.py
@@ -2,5 +2,10 @@ class TicketError(Exception):
     pass
 
 
+class CSVHandlerError(Exception):
+    pass
+
+
 if __name__ == "__main__":
-    raise TicketError("Test error")
+    # raise TicketError("Test error")
+    raise CSVHandlerError("some_wrong_dir")
